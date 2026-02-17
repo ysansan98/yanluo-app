eb2e64eac33840080190e326b7adbfcbe64ec561
Ysansan <ysansan98@outlook.com>
2026-02-17 15:31:44 +0800

    chore: 添加 code-inspector-plugin 以增强开发体验

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,17 +1,26 @@
 import { resolve } from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
+import { codeInspectorPlugin } from 'code-inspector-plugin'
 import { defineConfig } from 'electron-vite'
 
-export default defineConfig({
-  main: {},
-  preload: {},
-  renderer: {
-    resolve: {
-      alias: {
-        '@renderer': resolve('src/renderer/src'),
+export default defineConfig(({ command }) => {
+  return {
+    main: {},
+    preload: {},
+    renderer: {
+      resolve: {
+        alias: {
+          '@renderer': resolve('src/renderer/src'),
+        },
       },
+      plugins: [
+        vue(),
+        tailwindcss(),
+        ...(command === 'build'
+          ? []
+          : [codeInspectorPlugin({ bundler: 'vite' })]),
+      ],
     },
-    plugins: [vue() as any, tailwindcss()],
-  },
+  }
 })
